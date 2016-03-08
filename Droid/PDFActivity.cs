c99a72ff3472037e2a67f0bcab878e507bd91776
Konstantin Azizov <azizovkostya97@gmail.com>
2016-03-08 17:25:29 +0200

    Add resizable rectangle
    Basic view that allows to select specific location and send coordinates

--- Droid/PDFActivity.cs
+++ Droid/PDFActivity.cs
@@ -9,14 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using PSPDFKit.Configuration.Theming;
-using PSPDFKit.Configuration.Activity;
-using PSPDFKit.Configuration.Page;
-using PSPDFKit;
-using PSPDFKit.UI;
 
-using PSPDFKit.Document;
-using PSPDFKit.Document.Library;
 using System.IO;
 
 namespace Trukman.Droid
@@ -24,14 +17,15 @@ namespace Trukman.Droid
 	[Activity (Name="com.trukman.ui.pdfactivity", Label = "PDFActivity", Icon = "@drawable/icon", Theme = "@style/AppTheme")]
 	public class PDFActivity : Activity
 	{
+        private DragRect dr;
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
-			// Активизация просмотрщика PDF
-			PSPDFKitGlobal.Initialize (this, GetString(Resource.String.pspdfkitLicenseKey));
+            SetContentView(Resource.Layout.PDF);
+            dr = FindViewById<DragRect>(Resource.Id.PDFView);
 
-			// Create your application here
 			ShowPdfDocument (Intent.Data);
 		}
 
@@ -44,26 +38,7 @@ namespace Trukman.Droid
 
 		void ShowPdfDocument (Android.Net.Uri docUri)
 		{
-			// Customize thumbnailBar color defaults
-			var thumbnailBarThemeConfiguration = new ThumbnailBarThemeConfiguration.Builder (this)
-				.SetBackgroundColor (Android.Graphics.Color.Argb (255, 52, 152, 219))
-				.SetThumbnailBorderColor (Android.Graphics.Color.Argb (255, 44, 62, 80))
-				.Build ();
-
-			// Show Document using PSPDFKit activity
-			var pspdfkitConfiguration = new PSPDFActivityConfiguration.Builder (ApplicationContext, GetString(Resource.String.pspdfkitLicenseKey))
-				.ScrollDirection (PageScrollDirection.Horizontal)
-				.ShowPageNumberOverlay ()
-				.ShowThumbnailGrid ()
-				.ShowThumbnailBar ()
-				.ThumbnailBarThemeConfiguration (thumbnailBarThemeConfiguration)
-				.Build ();
-
-			if (!PSPDFKitGlobal.IsOpenableUri (this, docUri)) {
-				ShowError ("This document uri cannot be opened \n " + docUri.ToString ());
-			}
-			else
-				PSPDFActivity.ShowDocument (this, docUri, pspdfkitConfiguration);
+            
 		}
 
 		void ShowError (string message = null)
---
+++ Droid/Views/DragRect.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Text;
+
+namespace Trukman.Droid
+{
+    public class DragRect : View
+    {
+        #region Private members
+        private Paint mRectPaint;
+
+        private int startX = 0;
+        private int startY = 0;
+        private int endX = 0;
+        private int endY = 0;
+        private Boolean mDrawRect;
+
+        private OnUpCallback mCallback = null;
+
+        #endregion
+
+        #region Constructors
+
+        public DragRect(Context context)
+            : base(context)
+        {
+            Initialize();
+        }
+
+        public DragRect(Context context, IAttributeSet attrs)
+            : base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public DragRect(Context context, IAttributeSet attrs, int defStyle)
+            : base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        #endregion
+
+        public interface OnUpCallback {
+            void onRectFinished(Rect rect);
+        }
+
+        public void setOnUpCallback(OnUpCallback callback) {
+            mCallback = callback;
+        }
+
+        private void Initialize()
+        {
+            mRectPaint = new Paint();
+            mRectPaint.SetARGB(255, 255, 0, 0);
+            mRectPaint.SetStyle(Paint.Style.Stroke);
+            mRectPaint.StrokeWidth = 5;
+        }
+
+        public override bool OnTouchEvent(MotionEvent e) {
+            switch (e.Action) {
+                case MotionEventActions.Down:
+                    mDrawRect = false;
+                    startX = (int)e.GetX();
+                    startY = (int)e.GetY();
+                    Invalidate();
+                    break;
+
+                case MotionEventActions.Move:
+                    int x = (int) e.GetX();
+                    int y = (int) e.GetY();
+
+                    if (!mDrawRect || Math.Abs(x - endX) > 5 || Math.Abs(y - endY) > 5) {
+                        endX = x;
+                        endY = y;
+                        Invalidate();
+                    }
+
+                    mDrawRect = true;
+                    break;
+
+                case MotionEventActions.Up:
+                    if (mCallback != null)
+                    {
+                        mCallback.onRectFinished(new Rect(Math.Min(startX, endX), Math.Min(startY, endY),
+                                Math.Max(endX, startX), Math.Max(endY, startX)));
+                    }
+                    Invalidate();
+                    break;
+
+                default:
+                    break;
+            }
+
+            return true;
+        }
+            
+        protected override void OnDraw(Canvas canvas) {
+            base.OnDraw(canvas);
+
+            if (mDrawRect) {
+                canvas.DrawRect(Math.Min(startX, endX), Math.Min(startY, endY),
+                    Math.Max(endX, startX), Math.Max(endY, startY), mRectPaint);
+            }
+        }
+    }
+}
+
