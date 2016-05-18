d3e9dd810348572d7713854cf7e1ebd41dca22b7
Kevin (Rudy) Mathews <admin@Mac-Admin-2.local>
2016-05-18 23:58:04 +0300

    add annotation on map

---
+++ KAS.Trukman.iOS/Native ViewControllers/ContractorInfoViewController.designer.cs
@@ -0,0 +1,93 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace KAS.Trukman.iOS
+{
+	[Register ("ContractorInfoViewController")]
+	partial class ContractorInfoViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel addressLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel addressValueLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel contractorTypeLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel faxLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel faxValueLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel nameLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel nameValueLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel phoneLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel phoneValueLabel { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (addressLabel != null) {
+				addressLabel.Dispose ();
+				addressLabel = null;
+			}
+			if (addressValueLabel != null) {
+				addressValueLabel.Dispose ();
+				addressValueLabel = null;
+			}
+			if (contractorTypeLabel != null) {
+				contractorTypeLabel.Dispose ();
+				contractorTypeLabel = null;
+			}
+			if (faxLabel != null) {
+				faxLabel.Dispose ();
+				faxLabel = null;
+			}
+			if (faxValueLabel != null) {
+				faxValueLabel.Dispose ();
+				faxValueLabel = null;
+			}
+			if (nameLabel != null) {
+				nameLabel.Dispose ();
+				nameLabel = null;
+			}
+			if (nameValueLabel != null) {
+				nameValueLabel.Dispose ();
+				nameValueLabel = null;
+			}
+			if (phoneLabel != null) {
+				phoneLabel.Dispose ();
+				phoneLabel = null;
+			}
+			if (phoneValueLabel != null) {
+				phoneValueLabel.Dispose ();
+				phoneValueLabel = null;
+			}
+		}
+	}
+}
